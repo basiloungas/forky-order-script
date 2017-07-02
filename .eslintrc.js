f1170771086b3a541cfc15e7b2fc34da137b0bfd
Bill Trikalinos <billtrik@gmail.com>
2017-07-02 18:37:08 +0200

    Add babel-plugin-transform-class-properties dep and fix linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,10 @@
 module.exports = {
-    "extends": "airbnb-base"
+    "extends": "airbnb-base",
+    "parser": "babel-eslint",
+    "rules": {
+      "class-methods-use-this": 0,
+      "no-console": 0,
+      "import/prefer-default-export": 0,
+      "strict": 0
+    }
 };
